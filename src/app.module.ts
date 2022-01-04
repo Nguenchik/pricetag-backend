2df2383cecf4b0631faf054f0eec89f295f77de9
Your Name <you@example.com>
2022-01-04 19:18:22 +0300

    create

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,7 +3,8 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { UsersModule } from './users/users.module';
-import * as process from "process";
+import { CategoryModule } from './category/category.module';
+import * as process from 'process';
 
 const username = process.env.POSTGRES_USER || 'postgres';
 const password = process.env.POSTGRES_PASSWORD || 'example';
@@ -13,14 +14,15 @@ const password = process.env.POSTGRES_PASSWORD || 'example';
     TypeOrmModule.forRoot({
       type: 'postgres',
       host: 'localhost',
-      port: 7777,
+      port: 7770,
       username,
       password,
-      database: 'postgres',
+      database: 'restaurantdb',
       entities: [__dirname + '/**/*.entity{.ts,.js}'],
       synchronize: true,
     }),
     UsersModule,
+    CategoryModule,
   ],
   controllers: [AppController],
   providers: [AppService],
